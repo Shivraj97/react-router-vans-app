343d8e44413a5548b148699f6db1d144cde15a01
Shivraj Nag <43899907+Shivraj97@users.noreply.github.com>
2023-05-08 00:56:50 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import "./index.css";
+import About from "./pages/About";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link to="/">#Vanlife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
